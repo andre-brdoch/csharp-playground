53468ea20c4830758c075567bf5790c9eee4f68c
andre.brdoch <31482129+andre-brdoch@users.noreply.github.com>
2025-03-30 17:48:01 +0200

    practice pattern matching

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,9 +10,30 @@ namespace ConsoleApp1
             Calculator calc = new Calculator();
             int result = calc.add(1, 7);
             Console.WriteLine("Result is:" + result);
+
+            PatternMatcher patternMatcher = new PatternMatcher();
+            Console.WriteLine("Basic: True is " + patternMatcher.basic(true));
+            Console.WriteLine("Basic: False is " + patternMatcher.basic(false));
+            Console.WriteLine("Basic: False with fallback is " + patternMatcher.basicWithCatchAll(false));
+
         }
     }
 
+    internal class PatternMatcher
+    {
+        public string basic(bool a) => (a) switch
+        {
+            (true) => "correct",
+            (false) => "incorrect"
+        };
+
+        public string basicWithCatchAll(bool a) => (a) switch
+        {
+            (true) => "correct",
+            (_) => "incorrect"
+        };
+    }
+    
     internal class Calculator
     {
         public int add(int a, int b)

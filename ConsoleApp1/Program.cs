e4fac904128f91afa0fba1d25f3e07a76509b348
andre.brdoch <31482129+andre-brdoch@users.noreply.github.com>
2025-03-30 17:26:52 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Calculator calc = new Calculator();
+            int result = calc.add(1, 7);
+            Console.WriteLine("Result is:" + result);
+        }
+    }
+
+    internal class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
